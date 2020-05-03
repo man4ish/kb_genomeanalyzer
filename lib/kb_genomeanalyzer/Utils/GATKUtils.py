657c260aaf7e980e4fe2f35b25ce647f42fa8828
man4ish <mandecent.gupta@gmail.com>
2020-05-03 04:13:33 -0400

    lib modified

--- lib/kb_genomeanalyzer/Utils/GATKUtils.py
+++ lib/kb_genomeanalyzer/Utils/GATKUtils.py
@@ -1,43 +1,108 @@
 import os
 import subprocess
+import shutil
 
 from pprint import pprint,pformat
 
 class GATKUtils:
 
     def __init__(self):
-       self.callbackURL = os.environ['SDK_CALLBACK_URL']
+       self.path = "/kb/module/data/"
+       self.scratch = "/kb/module/work/tmp/"
+       #self.path = "/home/manish/Desktop/apps/kb_genomeanalyzer/data/"
+       #self.scratch = "/home/manish/Desktop/apps/kb_genomeanalyzer/test_local/workdir/tmp/"
        pass 
 
-    def deinterleave(self, fastq_file):
-        path = "/kb/module/work/tmp/"
-        fastq_1 = open(path + "r.fastq",'w')
-        fastq_2 = open(path + "f.fastq",'w')
-        [fastq_1.write(line) if (i % 8 < 4) else fastq_2.write(line) for i, line in enumerate(open(fastq_file))]
-        fastq_1.close()
-        fastq_2.close()
- 
-    def build_genome(assembly_file,  ):
-        return 
+    def run_cmd(self, cmd):
+        try:
+           process = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
+           stdout, stderr = process.communicate()
+           if stdout:
+               print ("ret> ", process.returncode)
+               print ("OK> output ", stdout)
+           if stderr:
+               print ("ret> ", process.returncode)
+               print ("Error> error ", stderr.strip())
 
-    def mapping_genome():
-        return 
+        except OSError as e:
+           print ("OSError > ", e.errno)
+           print ("OSError > ", e.strerror)
+           print ("OSError > ", e.filename)
+    
+    def build_genome(self, assembly_file ):
+        cmd = "bwa index -a bwtsw " + assembly_file
+        self.run_cmd(cmd)
+       
+    def index_assembly(self, assembly_file):
+        cmd = "samtools faidx " + assembly_file
+        self.run_cmd(cmd)
+      
+    def generate_sequence_dictionary(self, assembly_file):
+        cmd = "java -jar "+ self.path + "picard.jar CreateSequenceDictionary REFERENCE=" + assembly_file +" OUTPUT=" + assembly_file.replace("fa","dict")
+        self.run_cmd(cmd)
+               
+    def mapping_genome(self, ref_genome, rev_fastq, fwd_fastq ):
+        cmd = "bwa mem -t 32 -M -R " + "\"@RG\\tID:sample_1\\tLB:sample_1\\tPL:ILLUMINA\\tPM:HISEQ\\tSM:sample_1\" " + ref_genome +" "+ rev_fastq + " " + fwd_fastq + " > " + self.scratch + "aligned_reads.sam"
+        self.run_cmd(cmd)
+    
+    def duplicate_marking(self):
+        cmd = "java -jar "+ self.path + "picard.jar SortSam  INPUT= " + self.scratch + "aligned_reads.sam   OUTPUT=" + self.scratch + "aligned_reads.bam  SORT_ORDER=coordinate"
+        self.run_cmd(cmd)
+       
+    def sort_bam_index(self):
+        cmd = "samtools index " + self.scratch + "aligned_reads.bam"
+        self.run_cmd(cmd)
+        
+    def collect_alignment_and_insert_size_metrics(self, assembly_file):
+        cmd1 = "java -jar "+ self.path + "picard.jar CollectAlignmentSummaryMetrics R="+ assembly_file  +" I=" + self.scratch + "aligned_reads.bam O=" + self.scratch + "alignment_metrics.txt"
+        self.run_cmd(cmd1)
+        cmd2 = "java -jar "+ self.path + "picard.jar CollectInsertSizeMetrics INPUT=" + self.scratch + "aligned_reads.bam OUTPUT=" + self.scratch + "insert_metrics.txt HISTOGRAM_FILE=" + self.scratch + "insert_size_histogram.pdf"
+        self.run_cmd(cmd2)
+        cmd3 = "samtools depth -a " + self.scratch + "aligned_reads.bam > " + self.scratch + "depth_out.txt"
+        self.run_cmd(cmd3)
 
-    def duplicate_marking():
-        return
+    def variant_calling(self, assembly_file):
+        cmd = "java -jar "+ self.path + "gatk-4.1.3.0/gatk-package-4.1.3.0-local.jar HaplotypeCaller -R "+ assembly_file  + " -I " + self.scratch + "aligned_reads.bam -O " + self.scratch + "raw_variants.vcf"
+        self.run_cmd(cmd)
 
-    def indel_realignment():
-        return
+    def extract_variants(self, assembly_file ):
+        cmd1 = "java -jar "+ self.path + "gatk-4.1.3.0/gatk-package-4.1.3.0-local.jar SelectVariants -R " + assembly_file  + " -V " + self.scratch + "raw_variants.vcf --select-type SNP -O " + self.scratch + "raw_snps.vcf"
+        self.run_cmd(cmd1)
+        cmd2 = "java -jar "+ self.path + "gatk-4.1.3.0/gatk-package-4.1.3.0-local.jar SelectVariants -R " + assembly_file  + " -V " + self.scratch + "raw_variants.vcf --select-type INDEL -O " + self.scratch + "raw_indels.vcf"
+        self.run_cmd(cmd2)
 
-    def base_quality_score_recalibration():
-        return
+    def filter_SNPs(self, assembly_file, output_file):
+        cmd = "java -jar "+ self.path + "gatk-4.1.3.0/gatk-package-4.1.3.0-local.jar VariantFiltration -R " + assembly_file  + " -V " + self.scratch + "raw_snps.vcf -O "  + self.scratch +  output_file +" -filter-name 'QD_filter' -filter 'QD < 2.0' -filter-name 'FS_filter' -filter 'FS > 60.0' -filter-name 'MQ_filter' -filter 'MQ < 40.0' -filter-name 'SOR_filter' -filter 'SOR > 4.0' -filter-name 'MQRankSum_filter' -filter 'MQRankSum < -12.5' -filter-name 'ReadPosRankSum_filter' -filter 'ReadPosRankSum < -8.0'"
+        self.run_cmd(cmd)
 
-    def variant_calling():
-        return
+    def filter_Indels(self, assembly_file, output_file):
+        cmd = "java -jar "+ self.path + "gatk-4.1.3.0/gatk-package-4.1.3.0-local.jar VariantFiltration -R " + assembly_file  + " -V " + self.scratch + "raw_indels.vcf -O " + self.scratch + output_file +" -filter-name 'QD_filter' -filter 'QD < 2.0' -filter-name 'FS_filter' -filter 'FS > 200.0' -filter-name 'SOR_filter' -filter 'SOR > 10.0'"
+        self.run_cmd(cmd)
 
-    def genotype_refinement():
-        return
+    def exclude_filtered_variants(self):
+        cmd1 = "java -jar "+ self.path + "gatk-4.1.3.0/gatk-package-4.1.3.0-local.jar SelectVariants --exclude-filtered -V "  + self.scratch +"filtered_snps.vcf -O "  + self.scratch +"bqsr_snps.vcf"
+        self.run_cmd(cmd1)
 
-    def annotation():
-        return
+        cmd2 = "java -jar "+ self.path + "gatk-4.1.3.0/gatk-package-4.1.3.0-local.jar SelectVariants --exclude-filtered -V "  + self.scratch + "filtered_indels.vcf -O "  + self.scratch +"bqsr_indels.vcf"
+        self.run_cmd(cmd2)
 
+    def base_quality_score_recalibration(self, assembly_file, data_table):
+        cmd = "java -jar "+ self.path + "gatk-4.1.3.0/gatk-package-4.1.3.0-local.jar BaseRecalibrator -R " + assembly_file  + " -I "  + self.scratch + "aligned_reads.bam --known-sites "  + self.scratch + "bqsr_snps.vcf --known-sites "  + self.scratch + "bqsr_indels.vcf -O "  + self.scratch + data_table
+        self.run_cmd(cmd)
+
+    def apply_BQSR(self, assembly_file, data_table):
+        cmd = "java -jar "+ self.path + "gatk-4.1.3.0/gatk-package-4.1.3.0-local.jar ApplyBQSR -R " + assembly_file  + "  -I "  + self.scratch +"aligned_reads.bam -bqsr "+ self.scratch + data_table + " -O "  + self.scratch +"recal_reads.bam"
+        self.run_cmd(cmd)
+
+    def analyze_covariates(self):
+        #Error in library(gplots) : there is no package called ‘gplots’
+        cmd = "java -jar "+ self.path + "gatk-4.1.3.0/gatk-package-4.1.3.0-local.jar AnalyzeCovariates -before "  + self.scratch +"recal_data.table -after "  + self.scratch +"post_recal_data.table -plots "  + self.scratch +"recalibration_plots.pdf"
+        self.run_cmd(cmd)
+
+    def annotate_SNPs_and_predict_effects(self):
+        cmd = "java -jar "+ self.path + "snpEff/snpEff.jar -v Arabidopsis_thaliana "  + self.scratch +"filtered_snps_final.vcf > "  + self.scratch +"filtered_snps_final.ann.vcf"
+        self.run_cmd(cmd)
+    
+    def compile_statistics(self):
+        cmd = "parse_metrics.sh sample_id > sample_id_report.csv"
+        self.run_cmd(cmd)
--- lib/kb_genomeanalyzer/kb_genomeanalyzerImpl.py
+++ lib/kb_genomeanalyzer/kb_genomeanalyzerImpl.py
@@ -1,31 +1,132 @@
+# -*- coding: utf-8 -*-
 #BEGIN_HEADER
 import logging
 import os
-
+import shutil
+from kb_genomeanalyzer.Utils.GATKUtils import GATKUtils
 from installed_clients.KBaseReportClient import KBaseReport
 #END_HEADER
-#BEGIN_CLASS_HEADER
-#END_CLASS_HEADER
-#BEGIN_CONSTRUCTOR
+
+
+class kb_genomeanalyzer:
+    '''
+    Module Name:
+    kb_genomeanalyzer
+
+    Module Description:
+    A KBase module: kb_genomeanalyzer
+    '''
+
+    ######## WARNING FOR GEVENT USERS ####### noqa
+    # Since asynchronous IO can lead to methods - even the same method -
+    # interrupting each other, you must be *very* careful when using global
+    # state. A method could easily clobber the state set by another while
+    # the latter method is running.
+    ######################################### noqa
+    VERSION = "0.0.1"
+    GIT_URL = "https://github.com/man4ish/kb_genomeanalyzer.git"
+    GIT_COMMIT_HASH = "44ecb6fc4ade9f0269bd9d162aebc29e669cfd8d"
+
+    #BEGIN_CLASS_HEADER
+    #END_CLASS_HEADER
+
+    # config contains contents of config file in a hash or None if it couldn't
+    # be found
+    def __init__(self, config):
+        #BEGIN_CONSTRUCTOR
         self.callback_url = os.environ['SDK_CALLBACK_URL']
         self.shared_folder = config['scratch']
         logging.basicConfig(format='%(created)s %(levelname)s: %(message)s',
                             level=logging.INFO)
-#END_CONSTRUCTOR
-#BEGIN run_kb_genomeanalyzer
+        self.gu = GATKUtils()
+        #END_CONSTRUCTOR
+        pass
+
+
+    def run_kb_genomeanalyzer(self, ctx, params):
+        """
+        This example function accepts any number of parameters and returns results in a KBaseReport
+        :param params: instance of mapping from String to unspecified object
+        :returns: instance of type "ReportResults" -> structure: parameter
+           "report_name" of String, parameter "report_ref" of String
+        """
+        # ctx is the context object
+        # return variables are: output
+        #BEGIN run_kb_genomeanalyzer
+
         report = KBaseReport(self.callback_url)
         report_info = report.create({'report': {'objects_created':[],
                                                 'text_message': params['parameter_1']},
                                                 'workspace_name': params['workspace_name']})
+
+        self.scratch = "/kb/module/work/tmp"
+        src_assembly_file = "/kb/module/data/Athaliana_TAIR10.assembly.fa"
+        shutil.copy(src_assembly_file, self.scratch)
+        assembly_file = os.path.join(self.scratch,"Athaliana_TAIR10.assembly.fa")
+
+        self.gu.build_genome(assembly_file)
+
+        self.gu.index_assembly(assembly_file)
+
+        self.gu.generate_sequence_dictionary(assembly_file)
+
+        fwd_fastq = "/kb/module/data/arabidposis.1.fastq"
+        rev_fastq = "/kb/module/data/arabidposis.2.fastq"
+
+        self.gu.mapping_genome(assembly_file, fwd_fastq, rev_fastq )
+  
+        self.gu.duplicate_marking()
+compile_statistics
+        self.gu.sort_bam_index()
+
+        self.gu.collect_alignment_and_insert_size_metrics(assembly_file)
+   
+        #self.gu.analyze_covariates()
+   
+        self.gu.variant_calling(assembly_file)
+   
+        self.gu.extract_variants(assembly_file)
+   
+        self.gu.filter_SNPs(assembly_file, "filtered_snps.vcf")
+   
+        self.gu.filter_Indels(assembly_file, "filtered_indels.vcf")
+   
+        self.gu.exclude_filtered_variants()
+   
+        self.gu.base_quality_score_recalibration(assembly_file, "recal_data.table")
+   
+        self.gu.apply_BQSR(assembly_file, "recal_data.table")
+   
+        self.gu.base_quality_score_recalibration(assembly_file, "post_recal_data.table")
+   
+        self.gu.apply_BQSR(assembly_file,  "post_recal_data.table")
+   
+        self.gu.filter_SNPs(assembly_file, "filtered_snps_final.vcf")
+   
+        self.gu.filter_Indels(assembly_file, "filtered_indels_final.vcf")
+
+        self.gu.annotate_SNPs_and_predict_effects()
+
+        #self.gu.compile_statistics()
+
         output = {
             'report_name': report_info['name'],
             'report_ref': report_info['ref'],
         }
-#END run_kb_genomeanalyzer
-#BEGIN_STATUS
+        #END run_kb_genomeanalyzer
+
+        # At some point might do deeper type checking...
+        if not isinstance(output, dict):
+            raise ValueError('Method run_kb_genomeanalyzer return value ' +
+                             'output is not type dict as required.')
+        # return the results
+        return [output]
+    def status(self, ctx):
+        #BEGIN_STATUS
         returnVal = {'state': "OK",
                      'message': "",
                      'version': self.VERSION,
                      'git_url': self.GIT_URL,
                      'git_commit_hash': self.GIT_COMMIT_HASH}
-#END_STATUS
+        #END_STATUS
+        return [returnVal]
